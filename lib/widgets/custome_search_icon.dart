ea53541ac8e7957d111f2fbf9345a782a6b82ecf
mahmoud123asd <https://github.com/mahmoud123asd>
2025-05-01 01:14:14 +0200

    build custom_appbar

---
+++ lib/widgets/custome_search_icon.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class CustomeSearchIcon extends StatelessWidget {
+  const CustomeSearchIcon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 45,
+      width: 45,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          color: Colors.white.withValues(alpha: .05)),
+      child: Icon(
+        Icons.search,
+        size: 28,
+      ),
+    );
+  }
+}
