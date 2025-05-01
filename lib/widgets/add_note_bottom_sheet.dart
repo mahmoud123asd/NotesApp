fd5bdd1ff3d9cb8f0b90515642b8f527dc39c63d
mahmoud123asd <https://github.com/mahmoud123asd>
2025-05-02 01:26:47 +0200

    replace blocprovider in AddNoteBottomSheet

--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -15,23 +15,26 @@ class AddNoteBottomSheet extends StatefulWidget {
 class _AddNoteBottomSheetState extends State<AddNoteBottomSheet> {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: BlocConsumer<AddNoteCubit, AddNoteState>(
-        listener: (context, state) {
-          if (state is AddNoteFailure) {
-            print("failed");
-          } else if (state is AddNoteSuccess) {
-            Navigator.of(context).pop();
-          }
-        },
-        builder: (context, state) {
-          return ModalProgressHUD(
-              inAsyncCall: (state is AddNoteloading),
-              child: SingleChildScrollView(
-                child: AddNoteForm(),
-              ));
-        },
+    return BlocProvider(
+      create: (context) => AddNoteCubit(),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: BlocConsumer<AddNoteCubit, AddNoteState>(
+          listener: (context, state) {
+            if (state is AddNoteFailure) {
+              print("failed");
+            } else if (state is AddNoteSuccess) {
+              Navigator.of(context).pop();
+            }
+          },
+          builder: (context, state) {
+            return ModalProgressHUD(
+                inAsyncCall: (state is AddNoteloading),
+                child: SingleChildScrollView(
+                  child: AddNoteForm(),
+                ));
+          },
+        ),
       ),
     );
   }
