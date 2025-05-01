e56218eda31327486bef1e31bef4e23326ffffb7
mahmoud123asd <https://github.com/mahmoud123asd>
2025-05-01 15:21:38 +0200

    add button to notes bottom sheet

--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/custom_button%20.dart';
 import 'package:notes_app/widgets/custom_text_field.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
@@ -8,22 +9,31 @@ class AddNoteBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
-        children: [
-          SizedBox(
-            height: 30,
-          ),
-          CustomTextField(
-            hintText: "Title",
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          CustomTextField(
-            hintText: "Content",
-            maxLines: 5,
-          ),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 30,
+            ),
+            CustomTextField(
+              hintText: "Title",
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            CustomTextField(
+              hintText: "Content",
+              maxLines: 5,
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            CustomButton(),
+            SizedBox(
+              height: 10,
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/widgets/custom_button .dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/constants.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialButton(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+      minWidth: MediaQuery.of(context).size.width,
+      height: 45,
+      onPressed: () {},
+      color: kPrimaryColor,
+      child: Center(
+        child: Text(
+          "Add",
+          style: TextStyle(color: Colors.black, fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
