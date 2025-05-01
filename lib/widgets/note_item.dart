1f38eba8a49d29bd94a372f2aaa5f0a36e871a3a
mahmoud123asd <https://github.com/mahmoud123asd>
2025-05-01 15:29:59 +0200

    create edit note view

--- lib/widgets/note_item.dart
+++ lib/widgets/note_item.dart
@@ -1,53 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(bottom: 10),
-      padding: EdgeInsets.only(top: 16, bottom: 16, right: 16),
-      decoration: BoxDecoration(
-        color: Colors.yellow,
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        children: [
-          ListTile(
-            contentPadding: EdgeInsets.only(left: 16),
-            title: Text(
-              "flutter tips",
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 26,
+    return InkWell(
+      onTap: () {
+        Navigator.of(context)
+            .push(MaterialPageRoute(builder: (context) => EditNoteView()));
+      },
+      child: Container(
+        margin: EdgeInsets.only(bottom: 10),
+        padding: EdgeInsets.only(top: 16, bottom: 16, right: 16),
+        decoration: BoxDecoration(
+          color: Colors.yellow,
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          children: [
+            ListTile(
+              contentPadding: EdgeInsets.only(left: 16),
+              title: Text(
+                "flutter tips",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 26,
+                ),
+              ),
+              trailing: IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.delete,
+                    color: Colors.black,
+                    size: 30,
+                  )),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 16, bottom: 16),
+                child:
+                    Text("Flutter developer finish app  finish app  finish app",
+                        style: TextStyle(
+                          color: Colors.black.withValues(alpha: .5),
+                          fontSize: 15,
+                        )),
               ),
             ),
-            trailing: IconButton(
-                onPressed: () {},
-                icon: Icon(
-                  Icons.delete,
-                  color: Colors.black,
-                  size: 30,
-                )),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 16, bottom: 16),
-              child:
-                  Text("Flutter developer finish app  finish app  finish app",
-                      style: TextStyle(
-                        color: Colors.black.withValues(alpha: .5),
-                        fontSize: 15,
-                      )),
+            Align(
+              alignment: Alignment.bottomRight,
+              child: Text(
+                "1/1/2004",
+                style: TextStyle(color: Colors.black.withValues(alpha: .5)),
+              ),
             ),
-          ),
-          Align(
-            alignment: Alignment.bottomRight,
-            child: Text(
-              "1/1/2004",
-              style: TextStyle(color: Colors.black.withValues(alpha: .5)),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
